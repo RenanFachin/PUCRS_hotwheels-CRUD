7086816de4e24c3d2c84b55f395788609e177329
Renan <renan.fachin@hotmail.com>
2024-02-18 16:50:03 -0300

    style: Adiciona animações de adicionar ou deletar um card

--- src/components/car-list.tsx
+++ src/components/car-list.tsx
@@ -1,3 +1,4 @@
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 import { ReactNode } from 'react'
 
 interface CarListProps {
@@ -5,8 +6,13 @@ interface CarListProps {
 }
 
 export function CarList({ children }: CarListProps) {
+  const [parent] = useAutoAnimate()
+
   return (
-    <div className="grid max-h-[660px] min-h-[550px] grid-cols-3 gap-8 overflow-y-scroll scrollbar-thin scrollbar-track-primary-100 scrollbar-thumb-highlight-300/20">
+    <div
+      className="grid max-h-[660px] min-h-[550px] grid-cols-3 gap-8 overflow-y-scroll scrollbar-thin scrollbar-track-primary-100 scrollbar-thumb-highlight-300/20"
+      ref={parent}
+    >
       {children}
     </div>
   )
