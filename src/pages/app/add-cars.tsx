3cf474a64e9bb374124a0cb0971c0fb38704c80c
Renan <renan.fachin@hotmail.com>
2024-02-20 20:58:08 -0300

    fix: Correções para deploy

--- src/pages/app/add-cars.tsx
+++ src/pages/app/add-cars.tsx
@@ -1,22 +1,8 @@
 import { Helmet } from 'react-helmet-async'
-import { toast } from 'sonner'
 
-import { CarForm, CarFormSchema } from '@/components/car-form'
+import { CarForm } from '@/components/car-form'
 
 export function AddCars() {
-  // ADICIONNANDO UM REGISTRO
-  async function handleAddCar(data: CarFormSchema) {
-    try {
-      await new Promise((resolve) => setTimeout(resolve, 1000))
-
-      console.log(data)
-
-      toast.success('Carro adicionar com sucesso!')
-    } catch {
-      toast.error('Não foi possível cadastrar.')
-    }
-  }
-
   return (
     <div className="mx-auto flex w-full max-w-5xl flex-col items-center space-y-5 py-12">
       <Helmet title="Adicionar carros" />
